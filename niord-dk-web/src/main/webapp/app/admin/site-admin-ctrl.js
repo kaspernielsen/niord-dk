a6abddf6b9a978616d1c3b1db8a57f708bfa1a0e
peder280370 <peder@carolus.dk>
2016-02-18 10:11:18 +0100

    Refactoring and template AtoN import page

---
+++ niord-dk-web/src/main/webapp/app/admin/site-admin-ctrl.js
@@ -0,0 +1,15 @@
+
+/**
+ * The site admin functionality.
+ */
+angular.module('niord.admin')
+
+    /**
+     * Site Admin Controller
+     */
+    .controller('AtonIntegrationCtrl', ['$scope', '$location', '$uibModal',
+        function ($scope, $location, $uibModal) {
+            'use strict';
+
+
+        }]);
